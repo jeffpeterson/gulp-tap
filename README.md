70e4175cc5956f9d84f7c19c49145f031947d90d
Jeff Peterson <jeff@petersonj.com>
2014-04-14 12:30:37 -0700

    change t.through api

--- README.md
+++ README.md
@@ -2,32 +2,35 @@
 
 Easily tap into a pipeline.
 
-## Uses
+## Usage
 
 Some filters like `gulp-coffee` process all files. What if you want to process
-all JS and Coffee files in a single pipeline. Use tap to filter out '.coffee'
+all JS and Coffee files in a single pipeline. Use tap to filter out `.coffee`
 files and process them through coffee filter and let JavaScript files passthrough.
 
-    gulp.src("src/**/*.{coffee,js}")
-        .pipe(tap(function(file, t) {
-            if (path.extname(file.path) === '.coffee') {
-                return t.through(coffee, []);
-            }
-        }))
-        .pipe(gulp.dest('build'));
+```javascript
+gulp.src("src/**/*.{coffee,js}")
+  .pipe(tap(function(file, t) {
+    if (path.extname(file.path) === '.coffee') {
+      return t.through(coffee, {bare: true});
+    }
+  }))
+  .pipe(gulp.dest('build'));
+```
 
 What if you want to change content like add a header? No need for a separate
-filter, just chage the content.
+filter, just change the content.
 
-    tap(function(file) {
-        file.content = Buffer.concat([
-            new Buffer('HEADER'),
-            file.content
-        ]);
-    });
+```javascript
+tap(function(file) {
+  file.content = Buffer.concat([
+    new Buffer('HEADER'),
+    file.content
+  ]);
+});
+```
 
 If you do not return a stream, tap forwards your changes.
-
 
 ## License
 
--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-module.exports = require('./lib/tap.js');
+var ES = require('event-stream');
+var baseStream = require('stream');
+
+module.exports = function(lambda) {
+  var modifyFile;
+  var id = 1;
+  var cache = {};
+  var utils = function(tapStream, file) {
+    return {
+      /*
+      # Routes through another stream. The filter must not be
+      # created. This will create the filter as needed and
+      # cache when it can.
+      #
+      # @param filter {stream}
+      # @param args {Array} Array containg arguments to apply to filter.
+      #
+      # @example
+      #   t.through coffee, [{bare: true}]
+      */
+
+      through: function(filter) {
+        var args = [].slice.call(arguments, 1);
+        var stream;
+        if (filter.__tapId) {
+          stream = cache[filter.__tapId];
+          if (!stream) {
+            cache[filter.__tapId] = null;
+          }
+        }
+        if (stream) {
+
+        } else {
+          stream = filter.apply(null, args);
+          stream.on("error", function(err) {
+            return tapStream.emit("error", err);
+          });
+          filter.__tapId = "" + id;
+          cache[filter.__tapId] = stream;
+          id += 1;
+          stream.pipe(tapStream);
+        }
+        stream.write(file);
+        return stream;
+      }
+    };
+  };
+  modifyFile = function(file) {
+    var obj;
+    obj = lambda(file, utils(this, file));
+    if (!(obj instanceof baseStream)) {
+      return this.emit('data', file);
+    }
+  };
+  return ES.through(modifyFile);
+};
+
+/*
+//@ sourceMappingURL=tap.map
+*/
